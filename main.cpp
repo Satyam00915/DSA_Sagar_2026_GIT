df6dfa76b5bf72b1b70e799b0d32badf8d12bb83
Satyam00915 <satyamupadhyay0495@gmail.com>
2026-01-22 12:36:21 +0530

    Diamond_Question

--- main.cpp
+++ main.cpp
@@ -1,28 +1,42 @@
-#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void diamond(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < n - i; j++){
+            cout << " ";
+        }
+        for (int k = 0; k < i * 2 - 1; k++) {
+            cout << "*";
+        }
+        for (int l = 0; l < n - i; l++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i = n-1; i > 0; i--) {
+        for (int j = 0; j < n - i; j++){
+            cout << " ";
+        }
+        for (int k = 0; k < i * 2 - 1; k++) {
+            cout << "*";
+        }
+        for (int l = 0; l < n - i; l++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main(){
     int n;
-    std::cin >> n;
+    cin >> n;
 
-    for(int i = 0; i < n; i++){
-        for(int j = n - 1 - i; j > 0; j--){
-            std::cout << " ";
+    for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n - i; i++) {
+            cout << " ";
         }
 
-        for(int j = 0; j < 2 * i + 1; j++){
-            std::cout << "*";
-        }
-        std::cout << std::endl;
-    }
-
-    for(int i = 0; i < n - 1; i++){
-        for(int j = 0; j <= i ; j++){
-            std::cout << " ";
-        }
-
-        for(int j = 0; j < 2 * (n - i) - 3; j++){
-            std::cout << "*";
-        }
-        std::cout << std::endl;
     }
 }
